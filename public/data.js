9ea962e7399d3de8f5fd291180629dab425ba33b
soukayna ben <soukabenlahbib@gmail.com>
2022-10-28 00:42:00 +0100

    some modification

---
+++ public/data.js
@@ -0,0 +1,121 @@
+
+const data=
+    {"curriculum":[
+    {
+     year:"2018-Present",
+     title:"Ph.D. Student",
+     description:"Cotutelle between the University Ibn Tofail, Kenitra, Morocco and the University Della Calabria, Rende, Italy."
+  },
+    {
+     year:"2016-2018",
+     title:"Master’s Degree of of Applied Mathematics",
+     description:"University Sidi Mohamed Ben Abdellah, Faculty Dhar Mahraz (Fez - Morocco)."
+  },
+    {
+     year:"2013-2016",
+     title:"Bachelor’s of Science in Mathematics and Applications (MA)",
+     description:"University Sidi Mohamed Ben Abdellah, Faculty of Sicence and Technology (Fez - Morocco)."
+  },
+],"research":"On my research I am focusing on the interpolation and approximation theory on scattered data. Specifically, I am working on the Shepard&apos;s method and its modifications and extensions to high dimensions.","publications":[
+
+    {
+      year:"2020",
+      title:"F. Dell'Accio, F. Di Tommaso, O. Nouisser, N. Siar",
+      descriptions:["Rational Hermite interpolation on six-tuples and scattered data"],
+      footer:" Applied Mathematics and Computation, 386: 125452, 2020 "
+  },
+    {
+      year:"2021",
+      title:"F. Dell'Accio, F. Di Tommaso, N. Siar, M. Vianello",
+      descriptions:["Numerical differentiation on scattered data through multivariate polynomial interpolation"],
+      footer:"BIT Numerical Mathematics, https://doi.org/10.1007/s10543-021-00897-6 "
+  },
+    {
+      year:"2021",
+      title:"F. Dell'Accio, F. Di Tommaso, O. Nouisser, N. Siar",
+      descriptions:["Solving Poisson equation with Dirichlet conditions through multinode Shepard operators"],
+      footer:" Computers and Mathematics with Applications, 98: 254–260BIT Numerical Mathematics, https://doi.org/10.1007/s10543-021-00897-6 " 
+  },
+    {
+      year:"2021",
+      title:"F. Dell'Accio, F. Di Tommaso, N. Siar",
+      descriptions:["On the numerical computation of bivariate Lagrange polynomials"],
+      footer:" Applied Mathematics Letters, 112: 106845"
+  }
+   ],"conferences":[
+    {
+      year:" 2021",
+      title:"J2SD 2021 - Journées Scientifiques des Systèmes Dynamiques 2021",
+      description:"El Jadida (Morocco), November 25-27, 2021",
+      footer:"Invited Speaker"
+  },
+    {
+      year:" 2021",
+      title:"ATMA2021 - Approximation: Theory, Methods and Applications",
+      description:"Reggio Calabria (Italy), November 10-12, 2021",
+      footer:"Contributed Speaker"
+  },
+    {
+      year:" 2021",
+      title:"DWCAA21 - 5th Dolomites Workshop on Constructive Approximation and Applications ",
+      description:"Virtual Conference, September 6-10, 2021",
+      footer:"Speaker for the Poster Session"
+  },
+    {
+      year:" SIMAI 2020+2021",
+      title:"Minisymposium \"New Trends and Applications in Approximation Theory\" ",
+      description:"Parma (Italy), August, 30 - September 3, 2021",
+      footer:"Invited Speaker"
+  },
+    {
+      year:" SIAM AN21 - SIAM Annual meeting",
+      title:"Minisymposium \"MS5 Inverse Distance Weighting-Partition of Unity Methods and Applications\"",
+      description:"Virtual Conference, July 19-23, 2021",
+      footer:"Invited Speaker"
+  },
+    {
+      year:" 2021",
+      title:"RITA PhD Seminars",
+      description:"Virtual Seminars, May 28, 2021",
+      footer:"Speaker"
+  },
+   
+   ],"awards":[
+    {
+      title:"SIAM Student Travel Awards ",
+      description:"For the participation to the SIAM Annual Meeting 2021",
+      footer:"Virtual Conference, July 19-23, 2021"
+  },
+    {
+      title:"Best Poster Award ",
+      description:"DWCAA21 - 5th Dolomites Workshop on Constructive Approximation and Applications ",
+      footer:"Virtual Conference, September 6-10, 2021"
+  },
+    {
+      title:"Kovalevskaya Grant for ICM2022",
+      description:"For the participation to the ICM2022",
+      footer:" Virtual Conference, July 06-14, 2022"
+  }
+   ],"fellowship":[
+    {
+      year:"2019",
+      title:"Erasmus+ international credit mobility (KA107)",
+      descriptions:["Research Fellow of the National Center for Scientific and Technical Research (CNRST-Morocco)"],
+      footer:" Supervisor: Prof. Otheman Nouisser"
+  },
+    {
+      year:"2021",
+      title:"Erasmus+ international credit mobility (KA107)",
+      descriptions:["University of Granada (Spain), Department of Applied Mathematics","Supervisor: Prof. Domingo Barrera Rosillo"],
+      footer:" May 2021 - July 2021"
+  },
+    {
+      year:"2021",
+      title:"SIAM Student Travel Awards ",
+      descriptions:["For the participation to the SIAM Annual Meeting 2021"],
+      footer:"Virtual Conference, July 19-23, 2021"
+  }
+   ]}
+
+
+export default data;
